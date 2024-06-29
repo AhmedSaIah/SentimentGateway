16d8d55a67f18dd2983b36ad5819f27eede21914
AhmedSaIah <ahmedsalah5446@gmail.com>
2024-06-30 02:05:54 +0300

    infering model fix

--- SentimentAnalysisModel/main.py
+++ SentimentAnalysisModel/main.py
@@ -1,13 +1,9 @@
 from .Inference import Sentiment
 
+weight_path: str = "/home/ahmed/sentiment/SentimentGateway/SentimentAnalysisModel/ModelsRepository/model.pt"
+model_name: str = 'distilbert-base-uncased'
 def infer(samples):
-
-# fix the path
-    weight_path: str = "/home/ahmed/sentiment/SentimentGateway/SentimentAnalysisModel/ModelsRepository/model.pt"
-    model_name: str = 'distilbert-base-uncased'
     model = Sentiment(weight_path, model_name)
-
-    
     outs = model(samples)
     
     return outs
